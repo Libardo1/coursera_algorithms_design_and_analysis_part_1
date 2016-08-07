89da314676fb7cfbfb38a3cf68a85444114b2a99
Zhuyi Xue <alfred532008@gmail.com>
2016-08-07 10:00:17 -0700

    replace recursive dfs with FIFO

--- week4/scc.py
+++ week4/scc.py
@@ -4,77 +4,117 @@ sys.setrecursionlimit(100000)    # default: 1000
 
 def read_graph(data_file, reverse=False):
     graph = {}
-    max_n = 0
+    vertexes = set()
+    n_edges = 0
 
     with open(data_file) as inf:
         for line in inf:
             v_in, v_out = [int(_) for _ in line.split()]
-            max_n = max(v_in, v_out, max_n)
+            vertexes.add(v_in)
+            vertexes.add(v_out)
             if reverse:
                 if v_out not in graph:
-                    graph[v_out] = [v_in]
+                    graph[v_out] = {v_in}
                 else:
-                    graph[v_out].append(v_in)
+                    graph[v_out].add(v_in)
             else:
                 if v_in not in graph:
-                    graph[v_in] = [v_out]
+                    graph[v_in] = {v_out}
                 else:
-                    graph[v_in].append(v_out)
-    return graph, max_n
+                    graph[v_in].add(v_out)
+            n_edges += 1
+    return graph, len(vertexes), n_edges
 
-DEBUG = False
+
+DEBUG = eval(sys.argv[1])
 
 if DEBUG:
     data_file = 'small_SCC.txt'
+    # data_file = 'small_SCC2.txt'
 else:
     # data_file = 'small_SCC.txt'
     data_file = 'SCC.txt'
 
-explored = []
-entrypoints = []
-fin_times = {}
-fin_t = 1
+# recursive dfs doesn't because it results in over 20000 recursions, use FIFO
+
+# explored = []
+# entrypoints = []
+# fin_times = {}
+# fin_t = 1
+
+# def dfs(graph, i):
+#     """
+#     :param i: node i
+#     """
+#     global fin_t
+#     explored.append(i)
+#     for j in graph[i]:
+#         if j not in explored and j in graph:
+#             dfs(graph, j)
+#         else:
+#             if not i in entrypoints:
+#                 fin_times[i] = fin_t
+#                 fin_t += 1
 
 
-def dfs(graph, i):
+def dfs(graph, start, visited=None, fin_ts=None, fin_t=0):
     """
-    :param i: node i
+    :param fin_t: the time when a vertex is fully explored
     """
-    global fin_t
-    global last_vertex
-    explored.append(i)
-    for j in graph[i]:
-        if j in graph and j not in explored:
-            dfs(graph, j)
-        else:
-            if not i in entrypoints:
-                fin_times[i] = fin_t
-                fin_t += 1
-                # print(fin_times)
-                # print(explored)
+    if visited is None:
+        visited = set()
 
-graph, max_n = read_graph(data_file, reverse=True)
-print(max_n)
+    if fin_ts is None:
+        fin_ts = {}
 
+    stack = [start]
+    while stack:
+        vertex = stack.pop()
+        if vertex not in visited:
+            visited.add(vertex)
+            if vertex in graph:
+                stack.extend(graph[vertex] - visited)
+        fin_t += 1
+        fin_ts[vertex] = fin_t
+    return visited, fin_ts, fin_t
+
+graph, n_vertexes, n_edges = read_graph(data_file, reverse=True)
+print('finished reading graph with '
+      '{0} vertexes and {1} edges'.format(n_vertexes, n_edges))
+
+print('===== first pass:')
 
 if DEBUG:
     print(graph)
 
-for i in range(max_n, 0, -1):
-    if i in graph and i not in explored:
+visited = set()
+fin_ts = {}
+fin_t = 0
+entrypoints = []
+for i in range(n_edges, 0, -1):
+    if i in graph and i not in visited:
         entrypoints.append(i)
-        dfs(graph, i)
+        visited, fin_ts, fin_t = dfs(graph, i, visited, fin_ts, fin_t)
 
-        for _ in reversed(explored):
-            if _ not in fin_times:
-                fin_times[_] = fin_t
-                fin_t += 1
+#         for _ in reversed(explored[explored.index(i):]):
+#             if _ not in fin_times:
+#                 fin_times[_] = fin_t
+#                 fin_t += 1
+
+#     if i % 100000 == 0:
+#         print('Processing Vertex {0}'.format(i))
+
 
 if DEBUG:
-    print('=' * 20)
-    print(explored)
-    print(fin_times)
-    print(entrypoints)
+    print('explored: ', visited)
+
+# if DEBUG:
+#     print('explored: ', explored)
+#     print('finishing times: ', fin_times)
+#     print('entrypoints: ', entrypoints)
+
+
+sys.exit()
 
 
 ###############################################################################
@@ -96,11 +136,13 @@ def dfs(graph, i):
             dfs(graph, j)
         else:
             if i not in leaders:
+                print(i, scc)
                 sccs.append(scc)
                 scc = set()
 
+graph, _, _ = read_graph(data_file)
 
-graph, _ = read_graph(data_file)
+print('===== second pass:')
 
 if DEBUG:
     print(graph)
@@ -112,10 +154,9 @@ for (i, _) in sorted(fin_times.items(), key=lambda x: x[1], reverse=True):
         dfs(graph, i)
 
 if DEBUG:
-    print('=' * 20)
-    print(explored)
-    print(fin_times)
-    print(leaders)
-    print(sccs)
+    print('explored: ', explored)
+    print('leaders: ', leaders)
+    print('SCCs: ', sccs)
+    # assert sccs == [{1, 4, 7}, {9, 3, 6}, {8, 2, 5}]
 
 print(sorted(map(lambda x: len(x), sccs), reverse=True)[:5])
